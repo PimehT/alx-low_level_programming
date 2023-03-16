6876fc57a7335bfad5d1f13a90533d2a5b74886b
PimehT <tareerepimeh@gmail.com>
2023-03-16 14:59:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 /*
- * A program that prints text.
+ * A function that prints the text programming is like a multilingual puzzle.
  */
 
- #include <stdio.h>
+#include <stdio.h>
 
- int main(void)
- {
-	char str[] = "Programming is like building a multilingual puzzle\n";
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(str);
 	return (0);
- }
+}
