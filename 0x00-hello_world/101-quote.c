9db44e544027619f82cceb652da2dd272a63a6ab
PimehT <tareerepimeh@gmail.com>
2023-03-16 16:41:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 /**
  * main - this is the main function
  *
- * Return: always 0.
+ * Return: always 1.
  */
 
 #include <stdio.h>
+#include <undstd.h>
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
