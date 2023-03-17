4a666e30db8476e1677516ac235731d41c606dac
PimehT <tareerepimeh@gmail.com>
2023-03-17 12:47:10 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+/**
+ * main - this is the main function
+ * Return: always 0.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+		if (i == 'z')
+		{
+			for (j = 'A' ; j <= 'Z' ; j++)
+				putchar(j);
+		}
+	putchar('\n');
+	}
+	return (0);
+}
