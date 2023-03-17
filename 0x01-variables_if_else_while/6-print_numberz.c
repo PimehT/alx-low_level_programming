ed04ba38b8259b59028bc700348bcbe622665ee1
PimehT <tareerepimeh@gmail.com>
2023-03-17 13:30:10 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
