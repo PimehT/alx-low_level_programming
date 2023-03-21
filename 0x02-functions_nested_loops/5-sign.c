7aeeeab4c930e9481ab8faf51b13937374eab7a9
PimehT <tareerepimeh@gmail.com>
2023-03-21 12:53:59 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an integer n
+ * @n: the integer n
+ * Return: on success 1
+ * On error, return -1, and errno is set appropriately
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+	} else
+	{
+		_putchar('-');
+	}
+	return (0);
+}
