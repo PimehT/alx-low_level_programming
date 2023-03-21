5ebef2d42a731f92d8fc7c9c83bf1d18140ccb9c
PimehT <tareerepimeh@gmail.com>
2023-03-21 16:37:28 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - the multiplication table function
+ * return: always 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int mult;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			mult = i * j;
+
+			if (mult < 10)
+				_putchar(' ');
+			else
+				_putchar((mult / 10) + '0');
+
+			_putchar((mult % 10) + '0');
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
