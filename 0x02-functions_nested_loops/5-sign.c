e909470d42bec8c2046fce3fb05d52ead9bbabf2
PimehT <tareerepimeh@gmail.com>
2023-03-21 13:32:48 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,22 +3,21 @@
 /**
  * print_sign - prints the sign of an integer n
  * @n: the integer n
- * Return: on success 1
- * On error, return -1, and errno is set appropriately
+ * Return: 1 is greater the zero, 0 is zero
+ * -1 is less than zero
  */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
+		if (n == 0)
+		{
+			_putchar('0');
+			return (0);
+		}
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-	{
-		_putchar('0');
-		_putchar('0');
-	} else
-	{
-		_putchar('-');
 	}
+	_putchar('-');
 	return (-1);
 }
