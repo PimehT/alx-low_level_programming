a04246b116bba00289ce1346898b6afb873e17de
PimehT <tareerepimeh@gmail.com>
2023-03-21 19:00:39 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	long j, k;
+
+	i = 0;
+	j = 1;
+	k = 2;
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (j == 0)
+			printf("%ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
