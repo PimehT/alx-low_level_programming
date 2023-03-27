45db3286ba746b454a529bced4096e1f29241c83
PimehT <tareerepimeh@gmail.com>
2023-03-27 07:27:30 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int j;
+
+	j = 0;
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		j++;
+	}
+
+	return (j);
+}
