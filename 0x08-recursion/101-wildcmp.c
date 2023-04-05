c305fdda9b3ac31474005ec70985a85da130ac03
PimehT <tareerepimeh@gmail.com>
2023-04-05 08:43:24 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - determines if two strings are identical
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: On success 1,
+ * otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == 0)
+		return (1);
+	if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+			return (1);
+	} else
+	{
+		if (*s1 == '\0')
+			return (0);
+		if (*s1 == *s2 || *s2 == '?')
+			return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
