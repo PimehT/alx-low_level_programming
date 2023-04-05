94903303020bb8c81945bcdb68255488d3bd909c
PimehT <tareerepimeh@gmail.com>
2023-04-05 08:54:02 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
 			return (1);
 		else if (*s1 == 0 && *(s2 + 1) == 0)
-			return (1);
+			return (0);
 	} else
 	{
 		if (*s1 == '\0')
