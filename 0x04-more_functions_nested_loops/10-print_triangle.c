ef40197fd35c9f3e4c2cc402e650376376fd77ae
PimehT <tareerepimeh@gmail.com>
2023-04-08 08:57:31 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,37 +2,28 @@
 
 /**
  * print_triangle - print # character to form triangle shape
- * @size: the number of times to be printed
- * _putchar - to print characters
+ * @size: size of the triangle in lines
+ *
  * Return: void
  */
-
 void print_triangle(int size)
 {
-	int i;
-	int j;
-	int c;
+	int i, j;
 
-	c = 0;
-	i = size - 1;
-	while (c < size)
+	if (size < 1)
+		_putchar('\n');
+	else
 	{
-		i = size - 1 - c;
-		j = c + 1;
-		while (i > 0)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(' ');
-			i--;
+			for (j = size - 1; j >= 0; j--)
+			{
+				if (j > i)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
 		}
-		while (j > 0)
-		{
-			_putchar('#');
-			j--;
-		}
-		_putchar('\n');
-		c++;
 	}
-
-	if (size <= 0)
-		_putchar('\n');
 }
