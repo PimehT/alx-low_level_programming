1590933394e75fca81e7f9009eac80d840a24b5c
PimehT <tareerepimeh@gmail.com>
2023-04-09 08:04:12 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: number of parameters on command line
+ * @argv: array of strings (command line parameters)
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	int i;
+
+	for (i = 1; i <= argc; i++)
+		printf("%d\n", i - 1);
+	return (0);
+}
