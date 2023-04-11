9a0adefefb9019bc7f7947c7781317de2f99949b
PimehT <tareerepimeh@gmail.com>
2023-04-11 06:26:16 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 	char *p;
 
 	p = (char *)malloc(sizeof(char) * size);
-	if (size == 0)
+	if (p == NULL)
 	{
-		p = NULL;
+		return (NULL);
 	} else
 	{
 		for (i = 0; i < size; i++)
