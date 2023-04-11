168173caef9c0071fc5a5d98e5acbbb27d4b113c
PimehT <tareerepimeh@gmail.com>
2023-04-11 08:49:18 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2[len2] != 0)
 		len2++;
-	if (s1 == 0 && s2 == 0)
+	if (s1 == NULL && s2 == NULL)
 	{
 		len = 2;
 		s3 = (char *)malloc(len * sizeof(char));
