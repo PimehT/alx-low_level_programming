dfacbbec9ce926efc325d09270766ba8c13a3382
PimehT <tareerepimeh@gmail.com>
2023-04-13 10:04:45 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,42 +12,40 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	unsigned int old_s = old_size, new_s = new_size;
+	unsigned int copy_size;
 	void *new_p;
 
-	if (new_s == old_s)
-		return (ptr);
-	if (ptr == NULL)
-		new_p = malloc(new_s);
-	if (new_s == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_s > old_s)
+
+	if (ptr == NULL)
 	{
 		new_p = malloc(new_size);
 		if (new_p == NULL)
 			return (NULL);
-		i = 0;
-		while (*((char *)ptr + i))
-		{
-			*((char *)new_p + i) = *((char *)ptr + i);
-			i++;
-		}
 	}
-	if (new_s < old_s)
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size > old_size)
+		copy_size = new_size;
+	else
+		copy_size = old_size;
+
+	new_p = malloc(copy_size);
+	if (new_p == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*((char *)ptr + i))
 	{
-		new_p = malloc(new_size);
-		if (new_p == NULL)
-			return (NULL);
-		i = 0;
-		while (*((char *)new_p + i))
-		{
-			*((char *)new_p + i) = *((char *)ptr + i);
-			i++;
-		}
+		*((char *)new_p + i) = *((char *)ptr + i);
+		i++;
 	}
+
 	free(ptr);
 	return (new_p);
 }
