3ecc8696bb0eec44c42d6693b1aba088275d16ce
PimehT <tareerepimeh@gmail.com>
2023-04-17 14:38:58 -0700

    dog.h

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's attribute
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: This struct defines a dog's attribute
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
