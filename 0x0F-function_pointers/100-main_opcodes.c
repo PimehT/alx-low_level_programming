ff826b42a7ecef443e2184d34e9d59a4dbcb01f3
PimehT <tareerepimeh@gmail.com>
2023-04-19 15:13:22 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * print_opcodes - print the opcodes
+ * @addr: the address to print
+ * @size: size
+ *
+ * Return: Nothing.
+ */
+void print_opcodes(char *addr, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%.2hhx", *(addr + i));
+		if (i < size - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
  * main - check the code
  * @argc: argument counter
  * @argv: argument array
@@ -27,10 +47,10 @@ int main(int argc, char **argv)
 		return (2);
 	}
 
-	p = (char *) main;
+	p = (char *) &main;
 
 	for (i = 0; i < n; i++)
-		printf("%02hhx ", p[i]);
+		printf("%.2hhx ", p[i]);
 
 	printf("\n");
 
