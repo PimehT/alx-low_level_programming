b875b381e878492b803a9a8752ddc2c342b1914d
PimehT <tareerepimeh@gmail.com>
2023-04-27 08:27:14 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds node to the end of the list
+ * @head: pointer to the head of the list
+ * @str: string element in struct
+ *
+ * Return: address of last node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	last_node = malloc(sizeof(list_t));
+	if (last_node == NULL)
+		return (NULL);
+
+	last_node->str = strdup(str);
+	if (last_node->str == NULL)
+	{
+		free(last_node);
+		return (NULL);
+	}
+
+	last_node->len = strlen(str);
+	last_node->next = NULL;
+	*head = last_node;
+
+	return (last_node);
+}
