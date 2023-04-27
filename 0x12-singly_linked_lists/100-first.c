81e4a52fac9cbdcbf16d484d9efeea3a1d879e26
PimehT <tareerepimeh@gmail.com>
2023-04-27 09:10:02 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * before_main - prints text before main().
+ *
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n")
+}
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	before_main();
+
+	return (0);
+}
