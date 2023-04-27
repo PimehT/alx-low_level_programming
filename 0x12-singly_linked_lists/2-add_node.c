9a20da85c2e3dba258f72a7ecb579ab267071d9f
PimehT <tareerepimeh@gmail.com>
2023-04-27 08:18:20 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,13 +9,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_list = malloc(sizeof(list_t));
+	list_t *new_list;
 
+	if (str == NULL)
+		return (NULL);
+
+	new_list = malloc(sizeof(list_t));
 	if (new_list == NULL)
 		return (NULL);
 
-	new_list->str = str;
+	new_list->str = strdup(str);
+	if (new_list->str == NULL)
+	{
+		free(new_list);
+		return (NULL);
+	}
 	new_list->len = strlen(str);
 	new_list->next = *head;
 	*head = new_list;
+
+	return (new_list);
 }
