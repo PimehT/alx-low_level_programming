a7cc76c233f9792171bebe0200fdf8b505fcd546
PimehT <tareerepimeh@gmail.com>
2023-04-27 08:50:14 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer to head
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	head = temp->next;
+	free(temp);
+}
