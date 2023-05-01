dde96fd0a4f2574f2f3a790110bde64b9d832a2d
PimehT <tareerepimeh@gmail.com>
2023-05-01 10:38:15 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,6 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current->n = n;
 	previous->next = current;
 	current->next = next;
+	*head = previous;
 
 	return (current);
 }
