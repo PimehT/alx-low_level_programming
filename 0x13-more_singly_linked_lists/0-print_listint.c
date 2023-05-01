7dcad6eff20430cf3c830e17dfab33e2f55eea15
PimehT <tareerepimeh@gmail.com>
2023-05-01 07:30:30 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of alist
+ * @h: head pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i, count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	for (i = 0; i < count - 1; i++)
+	{
+		printf("%d\n", h->n);
+	}
+
+	return (count)
+}
