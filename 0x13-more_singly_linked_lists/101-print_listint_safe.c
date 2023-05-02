3b6c57bddb2107fd034e272c1be9fcd3eac402ac
PimehT <tareerepimeh@gmail.com>
2023-05-02 12:23:55 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,7 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *temp = head, *prev;
 
 	if (head == NULL)
-		exit(98);
+		return (98);
 	while (temp != NULL)
 	{
 		count++;
@@ -22,7 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (prev <= temp)
 		{
 			printf("-> [%p] %d\n", (void *)temp, temp->n);
-			exit(98);
+			return (98);
 		}
 	}
 
