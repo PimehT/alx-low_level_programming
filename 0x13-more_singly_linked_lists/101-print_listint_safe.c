debe30ea998e3bacdbdfa8237d1a5e7510cbc56a
PimehT <tareerepimeh@gmail.com>
2023-05-02 13:26:56 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,21 +9,45 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *temp = head, *prev;
+	const listint_t *current, *next;
 
 	if (head == NULL)
 		return (98);
-	while (temp != NULL)
+
+	current = next = head;
+
+	while (current && next && next->next)
 	{
+		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		prev = temp;
-		temp = temp->next;
-		if (prev <= temp)
+
+		current = current->next;
+		next = next->next->next;
+
+		if (current == next)
 		{
-			printf("-> [%p] %d\n", (void *)temp, temp->n);
+			printf("[%p] %d\n", (void *)current, current->n);
+			count++;
+
+			current = current->next;
+			while (current != next)
+			{
+				printf("[%p] %d\n", (void *)current, current->n);
+				count++;
+				current = current->next;
+			}
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			count++;
+
 			return (98);
 		}
+	}
+
+	while (current)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
 	}
 
 	return (count);
---
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint_safe(head2);
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint_safe(head);
+	free_listint_safe(&head2);
+	free_listint_safe(&head);
+	printf("%p, %p\n", (void *)head2, (void *)head);
+	return (0);
+}
