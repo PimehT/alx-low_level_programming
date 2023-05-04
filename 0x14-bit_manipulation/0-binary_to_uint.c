57ce4e6f6918f80d286d02cd357a59e5d1a7dcd1
PimehT <tareerepimeh@gmail.com>
2023-05-04 11:50:17 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts binary string to unsigned int
+ * binary_to_uint - converts binary string to decimal
  * @b: binary string
  *
  * Return: result
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the index of a number to 0
+ * @n: number to edit
+ * @index: index of number to edit
+ *
+ * Return: 1 on success, otherwise 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int))
+		return (-1);
+
+	n &= (1UL << index);
+	return (1);
+}
