ba428f221e71a3d2a220fbafbb00267df68f3a2e
PimehT <tareerepimeh@gmail.com>
2023-05-04 07:32:59 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _pow - returns exponetial of an int
+ * @x: int to exponetiate
+ * @y: the power
+ *
+ * Return: result
+ */
+unsigned int _pow(unsigned int x, unsigned int y)
+{
+	unsigned int result = 1;
+
+	if (x == 0)
+		return (0);
+
+	if (y == 0)
+		return (1);
+
+	result = x * _pow(x, (y - 1));
+
+	return (result);
+}
+
+/**
  * binary_to_uint - converts a binary to an unsigned int
  * @b: binary string
  *
@@ -8,7 +30,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int count = 0, decimal = 0, i = 0, j;
+	unsigned int count = 0, convert, decimal = 0, i = 0, j;
 
 	if (b == NULL)
 		return (0);
@@ -19,12 +41,12 @@ unsigned int binary_to_uint(const char *b)
 		i++;
 	}
 
-	for (i = 0; i < count; i++)
+	for (i = 0, j = count - 1; i < count; i++, j--)
 	{
-		for (j = count - 1; j >= 0; j--)
-		{
-			decimal += (b[i] - '0') * (2 ^ j);
-		}
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		convert = b[i] - 48;
+		decimal += (convert * _pow(2, j));
 	}
 
 	return (decimal);
