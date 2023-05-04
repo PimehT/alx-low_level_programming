8d8d86140eb453212bad5ef01c0a5e75f0dc5c35
PimehT <tareerepimeh@gmail.com>
2023-05-04 11:00:17 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,5 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	return ((n >> index) | 1);
+	return ((*n >> index) | 1);
 }
