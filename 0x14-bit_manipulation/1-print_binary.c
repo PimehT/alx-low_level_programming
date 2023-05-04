3067128c345b55e01eecb3f37f23d0a34628f493
PimehT <tareerepimeh@gmail.com>
2023-05-04 10:13:36 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,30 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned long int num;
+	int i = sizeof(n) * 8 - 1;
+	int lead_zeros = 1;
 
-	for (i = 0; d
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+
+	while (i >= 0)
+	{
+		if ((n >> i) & 1)
+		{
+			lead_zeros = 0;
+			_putchar('1');
+		}
+		else if (!lead_zeros)
+			_putchar('0');
+		i--;
+	}
 }
