15e7c843e67fc6a3b50cf725996a73ee63d0d09c
PimehT <tareerepimeh@gmail.com>
2023-05-04 14:03:16 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,6 +11,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count, i;
 
+	if (m == n)
+		return (0);
+
 	count = 0;
 	i = 0;
 	while (i < (sizeof(unsigned long int) * 8))
