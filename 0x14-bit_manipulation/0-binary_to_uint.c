dd10cc82eba6d1f7c909ef68a718e52093f77612
PimehT <tareerepimeh@gmail.com>
2023-05-04 07:45:31 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -43,7 +43,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0, j = count - 1; i < count; i++, j--)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if ((b[i] != '0') && (b[i] != '1'))
 			return (0);
 		convert = b[i] - 48;
 		decimal += (convert * _pow(2, j));
