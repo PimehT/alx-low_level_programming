9766b2218d04e79c08d27c711e97d6b7a7999485
PimehT <tareerepimeh@gmail.com>
2023-11-13 09:20:23 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-ops.c
@@ -0,0 +1,62 @@
+#include "libs.h"
+
+/**
+ * add - add two numbers together
+ * @a: first num
+ * @b: second num
+ *
+ * Returns: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts a number from another
+ * @a: first num
+ * @b: second num
+ *
+ * Returns: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Returns: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides a num by another
+ * @a: first num
+ * @b: second num
+ *
+ * Returns: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - divides a number by another
+ * @a: first num
+ * @b: second num
+ *
+ * Returns: the remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
