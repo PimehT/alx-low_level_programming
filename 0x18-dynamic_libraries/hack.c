4391bfec8a8d9bf0b78b1806675a835770ff3194
PimehT <tareerepimeh@gmail.com>
2023-11-13 13:45:23 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -7,4 +7,13 @@ int printf(const char *format, ...)
 	write(1, "9 8 10 24 75 9\n", 17);
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void srand(unsigned seed)
+{ }
+int rand()
+{
+	return (9);
 }
