59f9d17b6ff68175799a9dba07d7a0eae72b8ae2
PimehT <tareerepimeh@gmail.com>
2023-12-01 14:46:40 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,24 +17,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
-	new_node->value = (value != NULL) ? strdup(value) : strdup("");
-	if (new_node->value == NULL)
+	if (value != NULL)
 	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
+		new_node->value = strdup(value);
+		if (new_node->value == NULL)
+		{
+			free(new_node->key);
+			free(new_node);
+			return (0);
+		}
+	}
+	else
+	{
+		new_node->value = strdup("");
+		if (new_node->value == NULL)
+		{
+			free(new_node->key);
+			free(new_node);
+			return (0);
+		}
 	}
 
 	new_node->next = ht->array[index];
